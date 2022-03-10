11d6b97ddfd91b1a492d3f58474e979b85fd0843
ALESSANDRO ZAMPARUTTI s301132 <s301132@studenti.polito.it>
2022-03-10 11:09:38 +0100

    completed exercise

--- filmLibrary.js
+++ filmLibrary.js
@@ -24,24 +24,36 @@ function FilmLibrary(){
     };
 
     // delete film
-    this.deleteFile = (filmID) => {
+    this.deleteFilm = (filmID) => {
+        this.films.splice(this.films.findIndex(f => f.filmID === filmID), 1);
     };
 
+    // reset date of watched films
+    this.resetWatchedFilms = () => {
+        this.films.forEach(f => f.date = undefined);
+    };
+
+    // get Rated film
+    this.getRated = () => {
+        return this.films.filter(f => f.rating !== undefined).sort((f1, f2) => f2.rating - f1.rating);
+    };
 }
 
 const f1 = new Film(1, 'Pulp Fiction', true, dayjs('2022-3-10'), 5);
-const f2 = new Film(2, 'Grams', true, dayjs('2022-3-17'), 4);
+const f2 = new Film(2, 'Grams', true, dayjs('2022-3-17'), 3);
 const f3 = new Film(3, 'Star Wars', false);
 const f4 = new Film(4, 'Matrix', false);
-const f5 = new Film(5, 'Shrek', false, dayjs('2022-3-21'), 3);
+const f5 = new Film(5, 'Shrek', false, dayjs('2022-3-21'), 4);
 
-const Filmlib = new FilmLibrary();
-Filmlib.addNewFilm(f1);
-Filmlib.addNewFilm(f2);
-Filmlib.addNewFilm(f3);
-Filmlib.addNewFilm(f4);
-Filmlib.addNewFilm(f5);
+const filmLib = new FilmLibrary();
+filmLib.addNewFilm(f1);
+filmLib.addNewFilm(f2);
+filmLib.addNewFilm(f3);
+filmLib.addNewFilm(f4);
+filmLib.addNewFilm(f5);
 
-//console.log(Filmlib);
 //console.log(Filmlib.sortByDate());
-
+filmLib.deleteFilm(3);
+//filmLib.resetWatchedFilms();
+//console.log(filmLib);
+console.log(filmLib.getRated());
